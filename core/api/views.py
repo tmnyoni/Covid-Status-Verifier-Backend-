503b1fdb3999119822ccb797ad8374604b6ff230
Tawanda M. Nyoni <tmnyoni@outlook.com>
2022-03-10 02:11:07 +0200

    added more view methods.

--- core/api/views.py
+++ core/api/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.response import Response
@@ -13,10 +14,46 @@ class PeopleViewset(viewsets.ViewSet):
     def list(self, request):
         queryset = models.Person.objects.all()
         serializer = serializers.PersonSerializer(queryset, many=True)
-        return Response(serializer.data)
+        return Response(
+            serializer.data,
+            status=status.HTTP_200_OK
+        )
 
     def create(self, request):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(
+            serializer.data,
+            status=status.HTTP_201_CREATED
+        )
+
+    def retrieve(self, request, pk=None):
+        object_ = get_object_or_404(self.queryset, pk=pk)
+        serializer = self.serializer_class(object_)
+        return Response(
+            serializer.data,
+            status=status.HTTP_200_OK
+        )
+
+    def partial_update(self, request, pk=None):
+        object_ = get_object_or_404(self.queryset, pk=pk)
+        serializer = self.serializer_class(
+            object_,
+            data=request.data,
+            partial=True
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(
+            serializer.data,
+            status=status.HTTP_200_OK
+        )
+
+    def destroy(self, request, pk=None):
+        object_ = get_object_or_404(self.queryset, pk=pk)
+        object_.delete()
+        return Response(
+            data={"Success": "Deleted successfully"},
+            status=status.HTTP_204_NO_CONTENT
+        )
